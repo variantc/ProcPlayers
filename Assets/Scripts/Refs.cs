24fde224746293dae6fe7c7a1c2b4f2013530083
variantc <alboy_uk@hotmail.com>
2020-03-21 00:33:50 -0400

    added  static function to ref class

--- Assets/Scripts/Refs.cs
+++ Assets/Scripts/Refs.cs
@@ -5,4 +5,10 @@ using UnityEngine;
 public static class Refs  {
 
     public enum BehavState { FindSpace, FindBall, FindEnemy, PassBall, RunWithBall }
+
+    public static Vector3 FindFractionalPointBetween(Vector3 pos1, Vector3 pos2, float fraction)
+    {
+        Vector3 movePos = (pos2 - pos1) * fraction;
+        return movePos;
+    }
 }
